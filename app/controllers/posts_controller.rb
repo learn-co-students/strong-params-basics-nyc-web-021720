7a852738e078df2e4dbe1893dffb4a4ea1645830
codenameuriel <u.e.rodriguez@gmail.com>
2020-03-14 00:00:06 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,36 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
+	  # @post = Post.new(params["post"])
+		# @post = Post.new(params.require(:post).permit(:title, :description))
+		@post = Post.new(post_params(:title, :description))
+  	@post.save
+  	redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+		@post = Post.find(params[:id])
+		# @post.update(params["post"])
+		# @post.update(params.require(:post).permit(:title))
+		@post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+# DRY
+	private
+ 
+	# whitelisting attributes
+	def post_params(*args)
+	  params.require(:post).permit(*args)
+	end
 end
+
+# Strong Params
+
+# Permit vs. Require
+# Require means that the params that get passed in must contain a key called "post". If it's not included then it fails and the user gets an error
+# The permit method is a bit looser. It means that the params hash may have whatever keys are in it. So in the create case, it may have the :title and :description keys. If it doesn't have one of those keys it's no problem: the hash just won't accept any other keys.
